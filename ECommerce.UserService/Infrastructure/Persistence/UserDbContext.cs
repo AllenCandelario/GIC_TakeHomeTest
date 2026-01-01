49b5577e71053801210d1d2ca4e7f163b72cd906
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 07:32:08 +0800

    dbcontext

---
+++ ECommerce.UserService/Infrastructure/Persistence/UserDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ECommerce.UserService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.UserService.Infrastructure.Persistence
+{
+    public sealed class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Name)
+                    .IsRequired();
+
+                entity.Property(x => x.Email)
+                    .IsRequired();
+                entity.HasIndex(x => x.Email)
+                    .IsUnique();
+
+                entity.Property(x => x.CreatedAtUtc)
+                    .IsRequired();
+            });
+        }
+    }
+}
