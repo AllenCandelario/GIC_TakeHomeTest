481e976eadc5216b233d2212bb880509437a3cf3
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 16:02:18 +0800

    order service to publish a kafka event after order creation, and to receive a user created event after user creation. Currently event processing is a simple log

--- ECommerce.OrderService/Application/Services/OrderQueryHandler.cs
+++ ECommerce.OrderService/Application/Services/OrderQueryHandler.cs
@@ -1,16 +1,23 @@
 ﻿using ECommerce.OrderService.Application.Interfaces;
 using ECommerce.OrderService.Domain;
 using ECommerce.Shared.Exceptions;
+using ECommerce.Shared.Kafka.Contracts;
+using ECommerce.Shared.Kafka.Interfaces;
 
 namespace ECommerce.OrderService.Application.Services
 {
     public sealed class OrderQueryHandler : IOrderQueryHandler
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IKafkaProducer _kafkaProducer;
+        private readonly string _orderCreatedTopic;
 
-        public OrderQueryHandler(IOrderRepository orderRepository)
+        public OrderQueryHandler(IOrderRepository orderRepository, IKafkaProducer kafkaProducer, IConfiguration config)
         {
             _orderRepository = orderRepository;
+            _kafkaProducer = kafkaProducer;
+            _orderCreatedTopic = config["Kafka:Topics:OrderCreatedTopic"] ?? throw new InvalidOperationException("Kafka:Topics:OrderCreatedTopic missing");
+
         }
 
         public async Task<Order> AddOrderAsync(Guid userId, string product, int quantity, decimal price, CancellationToken ct)
@@ -20,6 +27,29 @@ namespace ECommerce.OrderService.Application.Services
             var order = new Order(userId, product, quantity, price);
             await _orderRepository.AddAsync(order, ct);
             await _orderRepository.SaveChangesAsync(ct);
+
+            // publish message after db write
+            var orderCreatedEvent = new OrderCreatedEventV1(order.Id, order.UserId, order.Product, order.Quantity, order.Price, order.CreatedAtUtc);
+    
+            var kafkaEvent = new KafkaEvent<OrderCreatedEventV1>(
+                EventId: Guid.NewGuid(),
+                EventType: _orderCreatedTopic,
+                Version: 1,
+                OccurredAtUtc: DateTime.UtcNow,
+                Producer: "order-service",
+                Key: order.UserId.ToString(), // key strategy = ordering per user
+                Data: orderCreatedEvent
+            );
+
+            try
+            {
+                await _kafkaProducer.PublishAsync(_orderCreatedTopic, kafkaEvent.Key, kafkaEvent, ct);
+            }
+            catch (Exception ex)
+            {
+                // dead stop here, no outbox or retry mechanisms for now
+                throw new ServiceUnavailableException($"Failed to publish order creation event: {ex.Message}, please retry");
+            }
 
             return order;
         }
---
+++ ECommerce.OrderService/Application/Services/UserCreatedEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Shared.Kafka.Contracts;
+using ECommerce.Shared.Kafka.Interfaces;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace ECommerce.OrderService.Application.Services
+{
+    public class UserCreatedEventHandler : IKafkaEventHandler
+    {
+        private readonly ILogger<UserCreatedEventHandler> _logger;
+
+        public UserCreatedEventHandler(ILogger<UserCreatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task HandleAsync(string key, string raw, CancellationToken ct)
+        {
+            var kafkaEvent = JsonSerializer.Deserialize<KafkaEvent<UserCreatedEventV1>>(raw);
+            if (kafkaEvent?.Data is null)
+            {
+                _logger.LogWarning("Invalid UserCreated event payload. Key={Key}", key);
+                return Task.CompletedTask;
+            }
+
+            _logger.LogInformation("Received UserCreated: UserId={UserId} Name={Name} Email={Email}",
+                kafkaEvent.Data.UserId, kafkaEvent.Data.Name, kafkaEvent.Data.Email);
+
+            return Task.CompletedTask;
+        }
+    }
+}
