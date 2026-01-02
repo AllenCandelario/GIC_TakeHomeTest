cc483c8bfc52b80fc7abbe5df0c0dc8fffbebf59
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 16:00:22 +0800

    kafka producer (to be used as a singleton service)

---
+++ ECommerce.Shared/Kafka/Interfaces/IKafkaProducer.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Shared.Kafka.Contracts;
+
+namespace ECommerce.Shared.Kafka.Interfaces
+{
+    public interface IKafkaProducer
+    {
+        Task PublishAsync<T>(string topic, string key, KafkaEvent<T> kafkaEvent, CancellationToken ct);
+    }
+}
