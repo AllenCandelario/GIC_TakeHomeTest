0405fd3d31a148c353fb9690a7fe29d330ecb505
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 15:59:13 +0800

    kafka contracts

---
+++ ECommerce.Shared/Kafka/Contracts/OrderCreatedEventV1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.Kafka.Contracts
+{
+    public sealed record OrderCreatedEventV1(Guid OrderId, Guid UserId, string Product, int Quantity, decimal Price, DateTime CreatedAtUtc);
+}
---
+++ ECommerce.Shared/Kafka/Contracts/UserCreatedEventV1.cs
@@ -0,0 +1,4 @@
+﻿namespace ECommerce.Shared.Kafka.Contracts
+{
+    public sealed record UserCreatedEventV1(Guid UserId, string Name, string Email, DateTime CreatedAtUtc);
+}
