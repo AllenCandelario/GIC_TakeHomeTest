15cf9624f3dcc40f678553010a5e2a172fbd21f3
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 15:59:35 +0800

    options pattern for kafka config

---
+++ ECommerce.Shared/Kafka/Options/KafkaConsumerConfigOptions.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+namespace ECommerce.Shared.Kafka.Options
+{
+    public sealed class KafkaConsumerConfigOptions
+    {
+        public required string BootstrapServers { get; init; }
+        public required string GroupId { get; init; }
+        public required string[] Topics { get; init; }
+
+        public AutoOffsetReset AutoOffsetReset { get; init; }
+        public bool EnableAutoCommit { get; init; }
+
+        public int MaxPollIntervalMs { get; init; }
+        public int SessionTimeoutMs { get; init; }
+    }
+}
