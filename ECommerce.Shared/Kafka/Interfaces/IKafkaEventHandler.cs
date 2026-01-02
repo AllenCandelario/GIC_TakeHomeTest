5d17dceca86e041beada64ae5900583d67ba4a68
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2026-01-02 16:00:46 +0800

    kafka event handler interface to be implemented by the respective services

---
+++ ECommerce.Shared/Kafka/Interfaces/IKafkaEventHandler.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ECommerce.Shared.Kafka.Interfaces
+{
+    public interface IKafkaEventHandler
+    {
+        Task HandleAsync(string key, string rawJson, CancellationToken ct);
+    }
+}
